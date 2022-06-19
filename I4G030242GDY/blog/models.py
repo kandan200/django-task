c996a4bfba4f7a7191c8a8e3a120ffc434819fce
kanu Chibuzo <kanuchibuzo200@gmail.com>
2022-06-19 12:51:23 +0100

    first push

---
+++ I4G030242GDY/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+            title=models.CharField(max_length=200)
+            text = models.TextField
+            created_date = models.DateTimeField
+            publisheddate = models.DateTimeField
+
+
+            def __str__(self):
+                        return self.title
